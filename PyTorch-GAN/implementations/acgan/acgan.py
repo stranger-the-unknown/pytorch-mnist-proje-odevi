22f5f9449b2466fbbc5c9f37a8f790f7c7ce0e00
stranger-the-unknown <81967554+stranger-the-unknown@users.noreply.github.com>
2022-06-12 02:21:35 +0300

    Update acgan.py

--- PyTorch-GAN/implementations/acgan/acgan.py
+++ PyTorch-GAN/implementations/acgan/acgan.py
@@ -128,7 +128,7 @@ discriminator.apply(weights_init_normal)
 
 # Configure data loader
 os.makedirs("../../data/FashionMNIST", exist_ok=True)
-dataloader = torch.utils.data.FashionMNIST(
+dataloader = torch.utils.data.DataLoader(
     datasets.FashionMNIST(
         "../../data/FashionMNIST",
         train=True,
